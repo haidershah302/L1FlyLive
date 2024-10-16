3b0c4897076d8ef65f95a58809469d19682b835d
haidershah302 <stfox302@gmail.com>
2024-10-16 11:54:42 -0700

    Checking server integration

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+
+    dd("You are not supposed to be here");
+//    return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
